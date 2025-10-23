b8751f6a397dbbb6e4299905a536b29e325e5023
JustAnotherBlueJay <93489878+JustAnotherBlueJay@users.noreply.github.com>
2025-10-23 14:21:28 +0100

    Added: basic character controller

---
+++ PlayerController.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public partial class PlayerController : CharacterBody2D
+{
+	[Export] private int playerID;
+
+	float speed = 300.0f;
+
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Get the input direction and handle the movement/deceleration.
+
+		Vector2 direction = GetInputDirection();
+
+        if (direction != Vector2.Zero)
+		{
+			velocity = new Vector2(direction.X * speed, direction.Y * speed);
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
+            velocity.Y = Mathf.MoveToward(Velocity.Y, 0, speed);
+
+        }
+
+        Velocity = velocity;
+		MoveAndSlide();
+	}
+
+	private Vector2 GetInputDirection()
+	{
+		switch (playerID)
+		{
+			case 0:
+				return Input.GetVector("player_1_move_left", "player_1_move_right", "player_1_move_up", "player_1_move_down");
+
+			case 1:
+				return Input.GetVector("player_2_move_left", "player_2_move_right", "player_2_move_up", "player_2_move_down");
+		}
+
+		return Vector2.Zero;
+	}
+}
