6fb18134e971a45718a61ea677ce7b32f718df1c
JustAnotherBlueJay <93489878+JustAnotherBlueJay@users.noreply.github.com>
2025-10-23 16:09:58 +0100

    Added: constellation points

---
+++ Scenes/Objects/ConstellationHole.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class ConstellationHole : Node2D
+{
+
+	private Area2D area;
+
+	
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		area = GetNode<Area2D>("./Area2D");
+
+		area.BodyEntered += OnBodyEntered;
+
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnBodyEntered(Node2D body)
+	{
+
+		if (body.GetGroups().Contains("Players"))
+		{
+			GD.Print("Player Entered");
+
+		}
+	}
+}
