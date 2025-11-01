a62eee70dd5d6c271fd897cc5df3b1e36c306ba0
BlueJay <flynntaylor1409@gmail.com>
2025-11-01 23:45:05 +0000

    Unlocking, Fade-In/Out, and Start positions

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 public partial class PlayerController : CharacterBody2D
 {
@@ -10,8 +11,13 @@ public partial class PlayerController : CharacterBody2D
 	float speed = 300.0f;
 	private bool active = true;
 
-
-	public override void _PhysicsProcess(double delta)
+    public override void _Ready()
+    {
+		//connecting to the game managers events
+		GameManager.E_ConstellationCompleted += OnConstellationCompleted;
+		GameManager.E_NewConstellationLoaded += OnNewConstellationLoaded;
+    }
+    public override void _PhysicsProcess(double delta)
 	{
 
 		if (!active)
@@ -60,4 +66,43 @@ public partial class PlayerController : CharacterBody2D
 
 		Position = starPos;
 	}
+
+	public async void OnConstellationCompleted()
+	{
+		//when a constelation is completed fade the player out then move them to the start position for the next constelation
+		await FadeOut();
+
+		//Update positions
+		Position = GameManager.GetPlayerStartPosition(playerID);
+
+	}
+
+	public async void OnNewConstellationLoaded()
+	{
+		//when a new constelation loads in show the player again and set them active
+		await FadeIn();
+
+		active = true;
+	}
+
+	//gradually fades the player out
+	private async Task FadeOut()
+	{
+		Tween tween = CreateTween();
+
+		tween.TweenProperty(this, "modulate:a", 0, 1.5f);
+		await ToSignal(tween, "finished");
+
+	}
+
+	//gradually fades the player in
+	private async Task FadeIn()
+	{
+		Tween tween = CreateTween();
+
+		tween.TweenProperty(this, "modulate:a", 1, 1.5f);
+		await ToSignal(tween, "finished"); 
+	}
+
+
 }
